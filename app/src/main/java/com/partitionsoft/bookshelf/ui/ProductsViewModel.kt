f45e6d91abc759f5210b9fbc9205f3f2068d7964
Haggai Khafizov <hafyok5777@gmail.com>
2024-03-12 13:33:21 +0300

    ViewModel is done (probably)

---
+++ app/src/main/java/com/partitionsoft/bookshelf/ui/ProductsViewModel.kt
@@ -0,0 +1,61 @@
+package com.partitionsoft.bookshelf.ui
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.partitionsoft.bookshelf.ProductsApplication
+import com.partitionsoft.bookshelf.data.ProductData
+import com.partitionsoft.bookshelf.data.ProductsRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface ProductsUiState {
+    data class Success(val productsSearch: List<ProductData>) : ProductsUiState
+    object Error : ProductsUiState
+    object Loading : ProductsUiState
+}
+
+class ProductsViewModel(
+    private val productsRepository: ProductsRepository
+) : ViewModel() {
+    var productsUiState: ProductsUiState by mutableStateOf(ProductsUiState.Loading)
+        private set
+
+    init {
+        getProducts()
+    }
+
+    fun getProducts(limitResults: Int = 20, maxResults: Int = 20) {
+        viewModelScope.launch {
+            productsUiState = ProductsUiState.Loading
+            productsUiState =
+                try {
+                    ProductsUiState.Success(productsRepository.getProducts(limitResults, maxResults))
+                } catch (e: IOException) {
+                    ProductsUiState.Error
+                } catch (e: HttpException) {
+                    ProductsUiState.Error
+                }
+        }
+    }
+
+
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ProductsApplication)
+                Log.d("Application key value", "Application key is $application")
+                val productsRepository = application.container.productsRepository
+                ProductsViewModel(productsRepository = productsRepository)
+            }
+        }
+    }
+}
