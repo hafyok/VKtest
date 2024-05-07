23f62dc0e9dfa6a8812b07159066f2897425d891
Haggai Khafizov <hafyok5777@gmail.com>
2024-05-07 15:29:18 +0300

    Fix UI, add little animation

--- app/src/main/java/com/partitionsoft/bookshelf/ui/screens/BooksGridScreen.kt
+++ app/src/main/java/com/partitionsoft/bookshelf/ui/screens/BooksGridScreen.kt
@@ -1,26 +1,40 @@
 package com.partitionsoft.bookshelf.ui.screens
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,33 +55,33 @@ fun PreviewBooksGridScreen() {
         viewModel(factory = ProductsViewModel.Factory)
     val product = listOf<ProductData>(
         ProductData(
-            "Title Test Title Test ",
-            "Description test",
+            "Title Test Title Test Title Test",
+            "Description test Description test Description test",
             "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
         ),
         ProductData(
-            "Title Test Title Test ",
-            "Description test",
+            "Title Test Title Test Title Test",
+            "Description test Description test Description test",
             "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
         ),
         ProductData(
-            "Title Test Title Test ",
-            "Description test",
+            "Title Test Title Test Title Test",
+            "Description test Description test Description test",
             "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
         ),
         ProductData(
-            "Title Test Title Test ",
-            "Description test",
+            "Title Test Title Test Title Test",
+            "Description test Description test Description test",
             "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
         ),
         ProductData(
-            "Title Test Title Test ",
-            "Description test",
+            "Title Test Title Test Title Test",
+            "Description test Description test Description test",
             "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
         ),
         ProductData(
-            "Title Test Title Test ",
-            "Description test",
+            "Title Test Title Test Title Test",
+            "Description test Description test Description test",
             "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
         ),
 
@@ -121,6 +135,7 @@ fun BooksGridScreen(
     }
 }
 
+
 @Composable
 fun BooksCard(
     product: ProductData,
@@ -128,7 +143,9 @@ fun BooksCard(
     //onBookClicked: (ProductData) -> Unit
 ) {
     Card(
-        modifier = modifier
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .shadow(elevation = 15.dp)
             .padding(4.dp)
             .fillMaxWidth()
             .requiredHeight(296.dp),
@@ -142,28 +159,21 @@ fun BooksCard(
                     .data(product.thumbnail?.replace("http:", "https:"))
                     .crossfade(true)
                     .build(),
-                error = painterResource(id = R.drawable.ic_book_96),
+                error = painterResource(id = R.drawable.vk_logo),
                 placeholder = painterResource(id = R.drawable.loading_img),
                 contentDescription = stringResource(id = R.string.content_description),
                 contentScale = ContentScale.Crop
             )
             product.title?.let {
-                Text(
-                    text = it,
-                    textAlign = TextAlign.Center,
-                    modifier = modifier
-                        .padding(top = 4.dp, bottom = 4.dp),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp
-                )
+                AnimatedTextReading(text = it)
             }
 
             product.description?.let {
                 Text(
                     text = it,
-                    textAlign = TextAlign.Center,
+                    textAlign = TextAlign.Start,
                     modifier = modifier
-                        .padding(top = 4.dp, bottom = 4.dp),
+                        .padding(4.dp),
                     //fontWeight = FontWeight.Bold,
                     fontSize = 14.sp
                 )
@@ -172,3 +182,47 @@ fun BooksCard(
     }
 }
 
+
+@Composable
+fun AnimatedTextReading(text: String) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val offsetX by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = -250f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(15000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        item {
+            Box(
+                modifier = Modifier
+                    .offset(x = offsetX.dp)
+            ) {
+                Text(
+                    text = text,
+                    modifier = Modifier.padding(2.dp),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        textAlign = TextAlign.Start
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAnimatedTextReading() {
+    val text = "This is a sample text for animated reading"
+    AnimatedTextReading(text = text)
+}
